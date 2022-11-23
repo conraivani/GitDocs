4f9e0a4273c79f0ded95a4a45876de688393a611
Conrado Ivani <conra_ivani@hotmail.com>
2022-11-23 19:04:39 -0300

    se agrega columna imagen

--- reactun1part1/backend/routes/admin/novedades.js
+++ reactun1part1/backend/routes/admin/novedades.js
@@ -1,17 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel');
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
+
 
 /* GET home page. */
 router.get('/',  async function (req, res, next) {
  
   var novedades = await novedadesModel.getNovedades();
 
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.image(novedad.img_id, {
+        width: 100,
+        height: 100,
+        crop: 'fill'
+      });
+      return {
+        ...novedad, imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: ''
+      }
+    }
+  });
+
   res.render('admin/novedades', {
     layout: 'admin/layout',
     usuario: req.session.nombre,
     novedades
-
   });
 });
 router.get('/agregar', (req, res, next) => {
@@ -24,9 +45,17 @@ router.get('/agregar', (req, res, next) => {
 
 router.post('/agregar', async(req, res, next) => {
   try {
+    var img_id = '';
+    if(req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen; 
+      img_id = (await uploader(imagen.tempFilePath)).public_id;
+    } 
+
     if (req.body.titulo != "" && req.body.subtitulo != "" &&
-    req.body.cuerpo != ""& req.body.link != "" ){
-      await novedadesModel.insertNovedad(req.body);
+    req.body.cuerpo != "" && req.body.link != "" ){
+      await novedadesModel.insertNovedad({
+        ...req.body, img_id
+      });
       res.redirect('/admin/novedades')
     } else {
       res.render('admin/agregar',{
